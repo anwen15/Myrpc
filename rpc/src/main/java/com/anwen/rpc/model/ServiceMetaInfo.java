f8d0e386e3b55ccdb8c9b369cfe0f52b67f914a8
anwen15 <2876709381@qq.com>
2024-04-02 22:09:35 +0800

    注册中心1

---
+++ rpc/src/main/java/com/anwen/rpc/model/ServiceMetaInfo.java
@@ -0,0 +1,51 @@
+package com.anwen.rpc.model;
+
+import java.io.PipedReader;
+
+/**
+ * @author nicefang
+ * @version 1.0
+ * @description: TODO
+ * @date 1/4/2024 下午11:22
+ * 服务元信息(注册)
+ */
+
+public class ServiceMetaInfo {
+
+    /**
+     * 服务名
+     */
+    private String servicename;
+
+    /**
+     * 服务版本号
+     */
+    private String serviceversion = "1.0";
+
+    /**
+     *服务地址
+     */
+    private String serviceaddress;
+
+    /**
+     * todo服务分组
+     */
+    private String servicegroup = "default";
+
+    /**
+     * 获取服务键名
+     * @return
+     */
+    public String getServicekey() {
+        return String.format("%s:%s", servicename, serviceversion);
+    }
+
+    /**
+     * 获取服务注册节点键名
+     * @return
+     */
+    public String getservicenodekey() {
+        return String.format("%s/%s", getServicekey(), serviceaddress);
+    }
+}
+
